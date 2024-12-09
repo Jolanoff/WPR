445a3fbfc3fd2a98b74e01ada025187f9c79d016
ronald <cleansernld@gmail.com>
2024-12-09 10:29:05 +0100

    add caravan seeder

--- backend/Program.cs
+++ backend/Program.cs
@@ -108,6 +108,8 @@ namespace backend
                 {
                     await SeedRoles.Seed(services);
                     await SeedAutos.Seed(services);
+                    await SeedCaravans.Seed(services);
+
                 }
                 catch (Exception ex)
                 {
