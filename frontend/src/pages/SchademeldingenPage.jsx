d4617f5159a327a013549342af233365f24d3baa
ronald <cleansernld@gmail.com>
2024-12-16 10:59:52 +0100

    update

--- frontend/src/pages/SchademeldingenPage.jsx
+++ frontend/src/pages/SchademeldingenPage.jsx
@@ -1,53 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import api from "../api";
-import UseAuthorization from '../utils/UseAuthorization';
+import UseAuthorization from "../utils/UseAuthorization";
 
 const SchademeldingenPage = () => {
   const [schademeldingData, setSchademeldingData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const { isAuthorized } = UseAuthorization(["BackOfficeMedewerker"])
+  const { isAuthorized } = UseAuthorization(["BackOfficeMedewerker"]);
 
   const handleStatusChange = async (id, newStatus) => {
     try {
-      const response = await api.post(`/Schademeldingen/switch_status/${id}?newStatus=${newStatus}`);
+      const response = await api.post(
+        `/Schademeldingen/switch_status/${id}?newStatus=${newStatus}`
+      );
       const updatedSchade = response.data;
 
       setSchademeldingData((prevData) =>
-        prevData.map((item) => (item.id === id ? { ...item, status: updatedSchade.status } : item))
+        prevData.map((item) =>
+          item.id === id ? { ...item, status: updatedSchade.status } : item
+        )
       );
-
+      fetchSchademeldingData();
     } catch (err) {
       console.error("Error switching status:", err);
       alert("Failed to update status.");
     }
   };
-
-
+  const fetchSchademeldingData = async () => {
+    try {
+      const response = await api.get("/Schademeldingen/get_all");
+      console.log(response.data);
+      const data = Array.isArray(response.data) ? response.data : [];
+      setSchademeldingData(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchSchademeldingData = async () => {
-      try {
-        const response = await api.get("/Schademeldingen/get_all");
-        console.log(response.data);
-        const data = Array.isArray(response.data) ? response.data : [];
-        setSchademeldingData(data)
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-
     fetchSchademeldingData();
-  }, [handleStatusChange]);
+  }, []);
 
- 
-
-
-  if (!isAuthorized) return <div>U bent niet bevoegd om deze pagina te bekijken.</div>;
+  if (!isAuthorized)
+    return <div>U bent niet bevoegd om deze pagina te bekijken.</div>;
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
@@ -70,13 +68,14 @@ const SchademeldingenPage = () => {
                 <th className="text-left py-2 px-4 border-b">Locatie</th>
                 <th className="text-left py-2 px-4 border-b">Status</th>
                 <th className=" py-2 px-4 border-b">Actions</th>
-
               </tr>
             </thead>
             <tbody>
               {schademeldingData.map((item, index) => (
                 <tr key={index}>
-                  <td className="py-2 px-4 border-b">{new Date(item.datum).toLocaleDateString()}</td>
+                  <td className="py-2 px-4 border-b">
+                    {new Date(item.datum).toLocaleDateString()}
+                  </td>
                   <td className="py-2 px-4 border-b">{item.beschrijving}</td>
                   <td className="py-2 px-4 border-b">{item.voertuigId}</td>
                   <td className="py-2 px-4 border-b">{item.merk}</td>
@@ -84,17 +83,20 @@ const SchademeldingenPage = () => {
                   <td className="py-2 px-4 border-b">{item.locatie}</td>
                   <td className="py-2 px-4 border-b">{item.status}</td>
 
-
                   <td className="py-2 px-4 border-b">
                     <div className="flex gap-2">
                       <button
-                        onClick={() => handleStatusChange(item.id, "InAfwachting")}
+                        onClick={() =>
+                          handleStatusChange(item.id, "InAfwachting")
+                        }
                         className="bg-yellow-400 text-white px-2 py-1 rounded hover:bg-yellow-500"
                       >
                         InAfwachting
                       </button>
                       <button
-                        onClick={() => handleStatusChange(item.id, "Goedgekeurd")}
+                        onClick={() =>
+                          handleStatusChange(item.id, "Goedgekeurd")
+                        }
                         className="bg-green-400 text-white px-2 py-1 rounded hover:bg-green-500"
                       >
                         Goedgekeurd
