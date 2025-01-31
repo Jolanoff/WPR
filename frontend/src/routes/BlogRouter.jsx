69d5dd2523eb06c23042c2df266e18513dabc3ce
Jolanoff <31005490+Jolanoff@users.noreply.github.com>
2025-01-31 14:27:16 +0100

    fix 2

--- frontend/src/routes/BlogRouter.jsx
+++ frontend/src/routes/BlogRouter.jsx
@@ -1,14 +1,11 @@
 import BlogPage from "../pages/BlogPage";
-import ProtectedRoute from "./ProtectedRoute";
+
 
 const BlogRouter = [
     {
         path: "/Blog",
-        element: (
-            <ProtectedRoute>
-                <BlogPage />
-            </ProtectedRoute>
-        ),
+        element: <BlogPage />
+        ,
     },
 ];
 
